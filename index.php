da7f3b30648b7622ba6fe3a28ab92c9b128442f5
diezit0 <diezito09xd@gmail.com>
2024-11-27 18:24:56 -0600

    agregando proyecto completo

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PROYECTO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+      .highlight-text {
+        font-size: 3em;
+        color: #ffffff;
+        text-shadow: 2px 2px 4px #000000;
+      }
+      .rotate {
+        animation: rotation 5s infinite linear;
+      }
+      @keyframes rotation {
+        from {
+          transform: rotate(0deg);
+        }
+        to {
+          transform: rotate(360deg);
+        }
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="container-fluid text-center" style="background:#ff4633;">
+      <h1 class="highlight-text">BIENVENIDO A ITSAO</h1>
+      <img src="Cliente\Captura de pantalla 2023-04-08 231834.png" width="100" height="100" class="mx-auto d-block rotate">
+    </div>
+
+    <div class="container" style="background:#ffcc99;">
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
